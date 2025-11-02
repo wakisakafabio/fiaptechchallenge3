c49f3f839f4fe237dc9f5e29ff954a10fdbbf07a
wakisakafabio <fabiowakisaka@gmail.com>
2025-11-02 15:55:41 -0300

    Create query4.sql
    4. Existe diferença significativa de churn entre gêneros?

---
+++ query4.sql
@@ -0,0 +1,7 @@
+SELECT 
+    gender,
+    COUNT(*) AS total_clientes,
+    SUM(churn) AS churn_count,
+    ROUND(100.0 * SUM(churn) / COUNT(*), 2) AS churn_percentage
+FROM bank_churn_fabiowakisaka_db.bank_churn_data
+GROUP BY gender;
