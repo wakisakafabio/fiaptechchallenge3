2f31f7055d5a7fbb47323cbd8bcfd188c3e9fe6b
wakisakafabio <fabiowakisaka@gmail.com>
2025-11-02 15:54:42 -0300

    Create query3.sql
    3. Qual a taxa de churn por país?

---
+++ query3.sql
@@ -0,0 +1,8 @@
+SELECT 
+    country,
+    COUNT(*) AS total_clientes,
+    SUM(churn) AS churn_count,
+    ROUND(100.0 * SUM(churn) / COUNT(*), 2) AS churn_percentage
+FROM bank_churn_fabiowakisaka_db.bank_churn_data
+GROUP BY country
+ORDER BY churn_percentage DESC;
